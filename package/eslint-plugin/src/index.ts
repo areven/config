c16024cdb09a61bdeb60dab785ce0784664ff674
Sebastian Nowak <sebastian-nowak@users.noreply.github.com>
2026-03-22 23:55:16 +0100

    style: reorder imports

--- package/eslint-plugin/src/index.ts
+++ package/eslint-plugin/src/index.ts
@@ -3,13 +3,13 @@
 // =============================================================================
 
 import fileHeader from './file-header';
-import importOrder from './import-order';
 import importLineBreaks from './import-line-breaks';
+import importOrder from './import-order';
 import type {ESLint} from 'eslint';
 
 
 export const rules: ESLint.Plugin['rules'] = {
   'file-header': fileHeader,
-  'import-order': importOrder,
-  'import-line-breaks': importLineBreaks
+  'import-line-breaks': importLineBreaks,
+  'import-order': importOrder
 };
